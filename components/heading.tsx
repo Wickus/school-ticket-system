557051f7af413e29f8fd755dbfcaa7c0f149595d
Wickus Van Der Merwe <vandermerwewickus@gmail.com>
2022-07-05 21:26:23 +0200

    Added the heading component

---
+++ components/heading.tsx
@@ -0,0 +1,10 @@
+const Heading: React.FC = () => {
+    return (
+        <div className="text-center">
+            <h1 className="text-5xl">Feetjieland</h1>
+			<h2 className="text-xl mt-3">Einstein Akademie</h2>
+        </div>
+    );
+};
+
+export default Heading;
