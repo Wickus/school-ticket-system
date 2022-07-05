838a1c412ac2806e6d593a380be7c7f15542b19b
Wickus Van Der Merwe <vandermerwewickus@gmail.com>
2022-07-05 21:26:13 +0200

    Added Layout

---
+++ components/layout.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from "react";
+
+const Layout: React.FC<PropsWithChildren> = ({ children }) => {
+    return (
+        <main className="bg-[url('/img/background.jpg')] min-h-[100vh] max-h-[100vh] overflow-auto bg-[position:-300px_-300px]">
+            <div className="w-full max-w-[1200px] bg-white/90 m-auto p-5">{children}</div>
+        </main>
+    );
+};
+
+export default Layout;
