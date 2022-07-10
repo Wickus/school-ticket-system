1c8fb886e677f02850b381588c2d3e12c9021a96
Wickus Van Der Merwe <vandermerwewickus@gmail.com>
2022-07-10 20:02:55 +0200

    Updated subTitle size and weight

--- components/heading.tsx
+++ components/heading.tsx
@@ -11,7 +11,7 @@ const Heading: React.FC = () => {
         <div className="relative z-10 text-center flex flex-col items-center justify-center h-44 bg-[url('/img/land-scape.jpg')] bg-cover">
             {/*  Content  */}
             <h1 className="text-5xl font-fairy-godmother tracking-wide">{title}</h1>
-            <h2 className="relative text-base top-2">{subTitle}</h2>
+            <h2 className="relative text-xl top-0 font-bold">{subTitle}</h2>
             {/*  Media  */}
             <div className="absolute w-28 h-24 top-[2px] left-[2px]">
                 <Image src={"/img/clipart/fairy-male.png"} layout="fill" />
