2d3727f30347404139db0c2db9641c3db325d2cb
Wickus Van Der Merwe <vandermerwewickus@gmail.com>
2022-07-10 17:47:11 +0200

    Updated the header of the web page

--- components/heading.tsx
+++ components/heading.tsx
@@ -1,8 +1,24 @@
+import Image from "next/image";
+
 const Heading: React.FC = () => {
+    /* Content */
+    const { title, subTitle } = {
+        title: "Feëtjieland",
+        subTitle: "Einstein Akademie",
+    };
+
     return (
-        <div className="text-center">
-            <h1 className="text-5xl">Feetjieland</h1>
-			<h2 className="text-xl mt-3">Einstein Akademie</h2>
+        <div className="text-center flex flex-col items-center justify-center h-44 bg-[url('/img/land-scape.jpg')] bg-cover">
+            {/*  Content  */}
+            <h1 className="text-5xl font-fairy-godmother tracking-wide">{title}</h1>
+            <h2 className="relative text-base top-2">{subTitle}</h2>
+            {/*  Media  */}
+            <div className="absolute w-28 h-24 top-[2px] left-[2px]">
+                <Image src={"/img/clipart/fairy-male.png"} layout="fill" />
+            </div>
+            <div className="absolute w-28 h-32 scale-x-[-1] top-6 right-1">
+                <Image src={"/img/clipart/fairy-shroom.png"} layout="fill" />
+            </div>
         </div>
     );
 };
